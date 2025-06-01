fa7cbe59ad9444236882cadec9e8594da8003e88
emblem-66 <143352594+emblem-66@users.noreply.github.com>
2025-06-01 16:04:45 +0200

    Add files via upload

---
+++ Instructions.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+set -ouex pipefail
+
+# Config files
+curl -s https://raw.githubusercontent.com/Emblem-66/kinoite/refs/heads/main/ConfigFiles.sh | bash
+
+# COPR repo add
+curl -sSL https://raw.githubusercontent.com/emblem-66/kinoite/refs/heads/main/dnf-repo.list | xargs -r dnf config-manager addrepo
+
+# COPR repo add
+curl -sSL https://raw.githubusercontent.com/emblem-66/kinoite/refs/heads/main/dnf-copr.list | xargs -r dnf copr enable -y
+
+# DNF remove packages
+curl -sSL https://raw.githubusercontent.com/emblem-66/kinoite/refs/heads/main/dnf-remove.list | xargs -r dnf remove -y
+
+# DNF install packages
+curl -sSL https://raw.githubusercontent.com/emblem-66/kinoite/refs/heads/main/dnf-install.list | xargs -r dnf install -y
+
+# DNF install packages
+curl -sSL https://raw.githubusercontent.com/emblem-66/kinoite/refs/heads/main/systemd.list | xargs -r systemctl enable
+
+# COPR repo remove
+curl -sSL https://raw.githubusercontent.com/emblem-66/kinoite/refs/heads/main/dnf-copr.list | xargs -r dnf copr remove -y
+
+# Tailscale repo remove
+#rm /etc/yum.repos.d/tailscale.repo
+
+# Check packages
+#curl -sSL https://raw.githubusercontent.com/emblem-66/kinoite/refs/heads/main/.list | xargs -r rpm -qa | sort | grep
+rpm -qa | sort | grep kernel
+rpm -qa | sort | grep mesa
+rpm -qa | sort | grep mutter
--- README.md
+++ README.md
@@ -1,1 +1,13 @@
-# kinoite
+# Silverblue
+
+Custom Fedora Silverblue image
+
+[![Silverblue](https://github.com/Emblem-66/Silverblue/actions/workflows/build.yml/badge.svg)](https://github.com/Emblem-66/Silverblue/actions/workflows/build.yml)
+
+```
+rpm-ostree rebase ostree-unverified-registry:ghcr.io/emblem-66/silverblue:latest
+```
+
+```
+sudo bootc switch ghcr.io/emblem-66/silverblue:latest
+```
